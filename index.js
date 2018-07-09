5659b8f21a6df31e0eaaaa3b4a508c00b73bf5df
Austin Mayden <austin.mayden@gmail.com>
2018-07-09 01:48:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete object[key];
+  var newObj = Object.assign({});
+  delete newObj[key];
   return newObj;
 }
 
